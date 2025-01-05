8eb68dcba416d64c2ac2b20b931562ec24bdf3a1
Ricard <rjuanjp98@gmail.com>
2025-01-05 13:00:15 +0100

    Adding the page roughly

--- django_intro/settings.py
+++ django_intro/settings.py
@@ -125,6 +125,7 @@ USE_TZ = True
 # Define the directory where collected static files will be stored
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
