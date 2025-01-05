5b72c33ff5a39f34a8f320fec5bc93b7f6d480e2
Ricard <rjuanjp98@gmail.com>
2025-01-05 11:29:16 +0100

    Some improvements

--- django_intro/django_intro/settings.py
+++ django_intro/django_intro/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'firewatch_home',
 ]
 
 MIDDLEWARE = [
@@ -115,7 +116,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = 'django_intro/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
---
+++ django_intro/firewatch_home/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class FirewatchHomeConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'firewatch_home'
