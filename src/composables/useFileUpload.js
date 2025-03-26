f7ac1c6375ff6575200826d5d05ff0f98c4c5bcd
Ricard <rjuanjp98@gmail.com>
2025-03-26 23:18:57 +0100

    Some stuff here and there

--- src/composables/useFileUpload.js
+++ src/composables/useFileUpload.js
@@ -1,51 +1,54 @@
 // composables/useFileUpload.js
-import { ref } from 'vue'
+import {ref} from 'vue'
 import {useGatewayService} from "@/composables/useGatewayService.js";
-
-const fileToUpload = ref(null) // shared reactive file
-var uploadedFile = null;
-var uploadFileURL = ""; //TODO: here set the upload url.
-
-async function tryUploadFile() {
-
-    if(!fileToUpload.value)
-    {
-        alert("Still no file selected for upload");
-        return;
-    }
-
-    uploadFileURL = await useGatewayService().getUploadURL()
-
-    if(uploadFileURL === '')
-    {
-        alert("Failed to create an upload url.")
-        return;
-    }
-
-    uploadedFile = await useGatewayService().uploadFileToUrl(uploadFileURL, fileToUpload)
-
-    if(!uploadedFile)
-    {
-        alert("File not uploaded.")
-        return;
-    }
-
-}
-
-function isFileUploaded() {
-    if(uploadedFile)
-    {
-        return true;
-    }
-    return false;
-}
-
-
-
 
 
 export function useFileUpload() {
+    const fileToUpload = ref(null) // shared reactive file
+    let uploadedFile = null;
+    let uploadFileURL = ""; //TODO: here set the upload url.
+
+    async function tryUploadFile() {
+
+        if (!fileToUpload.value) {
+            alert("Still no file selected for upload");
+            return;
+        }
+
+        uploadFileURL = await useGatewayService().getUploadURL()
+
+        if (isValidUrl(uploadFileURL)) {
+            alert(`Failed to create an upload url with ${uploadFileURL}`)
+            return;
+        }
+
+        uploadedFile = await useGatewayService().uploadFileToUrl(uploadFileURL, fileToUpload)
+
+        if (!uploadedFile) {
+            alert("File not uploaded.")
+            return;
+        }
+
+    }
+
+    function isFileUploaded() {
+        if (uploadedFile) {
+            return true;
+        }
+        return false;
+    }
+
+    function isValidUrl(string) {
+        try {
+            new URL(string);
+            return true;
+        } catch (_) {
+            return false;
+        }
+    }
+
+
     return {
-        tryUploadFile,uploadFileURL, isFileUploaded
+        tryUploadFile, uploadFileURL, isFileUploaded
     }
 }
