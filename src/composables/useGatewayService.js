79a588758caf543a552818a6eec036bb2ce15915
Ricard <rjuanjp98@gmail.com>
2025-03-26 23:03:00 +0100

    There we go

--- src/composables/useGatewayService.js
+++ src/composables/useGatewayService.js
@@ -2,6 +2,13 @@ import {startTask} from "@/composables/useTaskService.js";
 
 const TARGET_SERVER = "http://localhost:8000/";
 
+
+const startTaskEndpoint = `${TARGET_SERVER}/start_task`;
+const stopTaskEndpoint = `${TARGET_SERVER}/stop_task`;
+const getTaskStatusEndpoint = `${TARGET_SERVER}/get_task_status`;
+const getUploadUrlEndpoint = `${TARGET_SERVER}/get_upload_url`;
+
+
 export function useGatewayService() {
 
     async function startTask() {
@@ -10,13 +17,28 @@ export function useGatewayService() {
     async function stopTask() {
     }
 
-    async function fetchTaskStatus() {
+    async function getTaskStatus() {
     }
 
     async function getUploadURL() {
         const response = await fetch(TARGET_SERVER + "get_upload_url");
         let json_response = await response.json();
         return json_response.message;
+
+        fetch("http://localhost:8000/get_upload_url")
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error("Network response was not ok");
+                }
+                return response.json();
+            })
+            .then(data => {
+                console.log("Upload URL:", data.upload_url);
+            })
+            .catch(error => {
+                console.error("Fetch error:", error);
+            });
+
     }
 
     async function uploadFileToUrl(uploadFileURL, fileToUpload) {
@@ -30,5 +52,5 @@ export function useGatewayService() {
         return await response.json();
     }
 
-    return {getUploadURL, uploadFileToUrl, startTask, stopTask, fetchTaskStatus};
+    return {getUploadURL, uploadFileToUrl, startTask, stopTask, fetchTaskStatus: getTaskStatus};
 }
